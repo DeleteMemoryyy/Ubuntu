934de0cc2c57000914315c3a0b5dc4a27b96715e
DeleteMemory <xtliudexin@gmail.com>
2016-12-21 21:26:18 +0800

    init

--- Lab8-ProxyLab/proxylab-handout/proxy.c
+++ Lab8-ProxyLab/proxylab-handout/proxy.c
@@ -1,3 +1,4 @@
+#include "csapp.h"
 #include <stdio.h>
 
 /* Recommended max cache and object sizes */
@@ -5,10 +6,50 @@
 #define MAX_OBJECT_SIZE 102400
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char* user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux "
+                                    "x86_64; rv:10.0.3) Gecko/20120305 "
+                                    "Firefox/10.0.3\r\n";
 
-int main()
+void* thread(void* vargp);
+void process_request(int connfd);
+
+int main(int argc, char** argv)
 {
-    printf("%s", user_agent_hdr);
+
+    printf("%s\n", user_agent_hdr);
+
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    /* Check command line args */
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    listenfd = Open_listenfd(argv[1]);
+
+    while (1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfdp = Malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA*)&clientaddr, &clientlen);
+        Pthread_create(&tid, NULL, thread, connfdp);
+    }
+
     return 0;
 }
+
+void* thread(void* vargp)
+{
+    int connfd = *((int*)vargp);
+    Pthread_detach(Pthread_self());
+    Free(vargp);
+    process_request(connfd);
+    Close(connfd);
+    return NULL;
+}
+
+void process_request(int connfd) { printf("1\n"); }
